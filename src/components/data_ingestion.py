1ba08b6675f7d8d4da082324e54c44279d8eed2c
Jeevan85 <jeevanteja8367@gmail.com>
2023-09-16 12:23:38 +0530

    training pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -46,8 +46,8 @@ class DataIngestion:
         
 
 ##to run the data ingestion
-
+'''
 if __name__ =='__main__':
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-
+'''
